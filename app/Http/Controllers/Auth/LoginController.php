c78563fd2db85f193076e2c2fbc0e6e72927d9f3
alifnrz27 <alifnaufalrizki27@gmail.com>
2024-10-26 12:40:59 +0700

    Syncronize login page with backend

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Http;
 
 class LoginController extends Controller
 {
@@ -16,29 +17,49 @@ class LoginController extends Controller
     {
         // Validasi input
         $request->validate([
-            'email' => 'required|email',
+            'username' => 'required',
             'password' => 'required|min:8',
         ], [
-            'email.required' => 'Email wajib diisi.',
-            'email.email' => 'Email tidak valid.',
+            'username.required' => 'Username wajib diisi.',
             'password.required' => 'Password wajib diisi.',
-            'password.min' => 'Minimal 8 karakter.',
+            'password.min' => 'Password minimal 8 karakter.',
         ]);
 
-        // Data login
-        $credentials = $request->only('email', 'password');
+        // Data login dalam format JSON
+        $credentials = [
+            'username' => $request->username,
+            'password' => $request->password,
+        ];
 
-        // Proses autentikasi
-        if (Auth::attempt($credentials)) {
-            // Jika berhasil login, redirect ke halaman dashboard
+        // Kirim permintaan ke API
+        $response = Http::timeout(20)->withoutVerifying()->post('https://sensor-monitoring.apicollection.my.id/api/v1/authenticate', $credentials);
+
+        // Cek apakah respons sukses
+        if ($response->ok()) {
+            // Ambil data dari respons
+            $data = $response->json()['data'];
+
+            // Simpan token dan data pengguna di session
+            session([
+                'access_token' => $data['access_token'],
+                'clientId' => $data['clientId'],
+                'clientSecret' => $data['clientSecret'],
+                'expires_in' => $data['expires_in'],
+                'refresh_token' => $data['refresh_token'],
+                'scope' => $data['scope'],
+                'userId' => $data['userId'],
+            ]);
+
+            // Redirect ke halaman dashboard dengan pesan sukses
             return redirect(route('home'))->with('success', 'Login berhasil!');
         } else {
             // Jika gagal login, redirect kembali ke form login dengan pesan error
             return back()->withErrors([
-                'email' => 'Email atau password salah',
-            ])->withInput($request->only('email'));
+                'username' => 'Username atau password salah',
+            ])->withInput($request->only('username'));
         }
     }
+
 
     // Method untuk logout
     public function logout()
