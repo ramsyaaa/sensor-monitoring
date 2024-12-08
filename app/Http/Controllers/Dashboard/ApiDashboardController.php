cce4c4f564edb1724054f17e732707617da8cb3f
alifnrz27 <alifnaufalrizki27@gmail.com>
2024-12-08 16:29:12 +0700

    edit dashboard and maps

--- app/Http/Controllers/Dashboard/ApiDashboardController.php
+++ app/Http/Controllers/Dashboard/ApiDashboardController.php
@@ -40,72 +40,14 @@ class ApiDashboardController extends Controller
                         "active" => 0,
                         "total" => 0,
                     ];
+
                     foreach ($group as $index3 => $device) {
-                        if (isset($device['sensor_id']) && $device['sensor_id'] != null && $device['is_line'] == 1) {
+                        $allDevice[$index2][] = $device;
+                        if ($device['is_line'] == 1) {
                             $dataOverviewDevice[$index2]["active"] += 1;
-                            $dataOverviewDevice[$index2]["total"] += 1;
-                            $jsonData = [
-                                'userId' => 99837,
-                                'sensorId' => intval($device['sensor_id']),
-                            ];
-
-                            // $sensorResponse = Http::timeout(20)->withoutVerifying()
-                            //     ->withHeaders([
-                            //         'Authorization' => 'Bearer ' . $token,
-                            //         'Accept' => 'application/json',
-                            //         'Content-Type' => 'application/json',
-                            //     ])
-                            //     ->post(env('URL_API') . '/api/v1/get-single-sensor', $jsonData);
-
-                            $allDevice[$index2][] = [
-                                "device_id" => $device['id'],
-                                'device_name' => $device['device_name'],
-                                'group_name' => $device['group_name'],
-                                'is_line' => $device['is_line'],
-                                "sensor_name" => $device['sensor_name'],
-                                "point_code" => $device['point_code'],
-                                "address" => $device['address'],
-                                "value" => 0,
-                                'unit' => 'm',
-                                'updated_at' => null,
-                            ];
-
-                            // if ($sensorResponse->successful()) {
-                            //     $responseJson = $sensorResponse->json();
-                            //     $response_data_sensor = $responseJson['data'];
-
-                            //     $allDevice[$index2][] = [
-                            //         "device_id" => $device['id'],
-                            //         'device_name' => $device['device_name'],
-                            //         'group_name' => $device['group_name'],
-                            //         'is_line' => $device['is_line'],
-                            //         "sensor_name" => $device['sensor_name'],
-                            //         "point_code" => $device['point_code'],
-                            //         "address" => $device['address'],
-                            //         "value" => $response_data_sensor['value'],
-                            //         'unit' => $response_data_sensor['unit'],
-                            //         'updated_at' => $response_data_sensor['updateDate'],
-                            //     ];
-                            // } else {
-                            //     return response()->json([
-                            //         'success' => false,
-                            //         'message' => 'Gagal mengambil data dari API: ' . $sensorResponse->body(),
-                            //     ], 500);
-                            // }
+                            $dataOverviewDevice[$index2]["total"] += 1;                            
                         } else {
                             $dataOverviewDevice[$index2]["total"] += 1;
-                            $allDevice[$index2][] = [
-                                "device_id" => $device['id'],
-                                'device_name' => $device['device_name'],
-                                'group_name' => $device['group_name'],
-                                'is_line' => $device['is_line'],
-                                "sensor_name" => $device['sensor_name'],
-                                "point_code" => $device['point_code'],
-                                "address" => $device['address'],
-                                "value" => 0,
-                                'unit' => 'm',
-                                'updated_at' => null,
-                            ];
                         }
                     }
                 }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -32,7 +32,7 @@
                         @for ($i = 0; $i < 3; $i++)
                             <div class="flex flex-col gap-4 mb-8 p-4 bg-gray-200 rounded-lg shadow-lg animate-pulse">
                                 <div class="h-6 bg-gray-300 rounded w-1/4 mb-4"></div>
-                                <div class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-8 gap-4">
+                                <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4">
                                     @for ($j = 0; $j < 8; $j++)
                                     <div class="bg-gray-300 rounded p-4 shadow-lg">
                                         <div class="h-4 bg-gray-400 rounded mb-2"></div>
@@ -46,41 +46,6 @@
                         @endfor
                     </div>
                 </div>
-                {{-- <div class="overflow-x-auto bg-white px-6 py-10 rounded-lg shadow-lg">
-                    @foreach ($data_overview_device as $index => $data)
-                            <div class="bg-white shadow-lg rounded-lg p-6 text-center">
-                                <h2 class="text-lg font-bold text-gray-700">Total Online {{ $index }}</h2>
-                                <p class="text-2xl font-bold text-blue-500 mt-2">{{ $data['active'] }} / {{ $data['total'] }}</p>
-                            </div>
-                            @endforeach
-
-
-                    @foreach ($all_devices as $key => $group)
-                    <div class="flex flex-col gap-4 mb-8 p-4 bg-gray-200 rounded-lg shadow-lg">
-                        <div class="font-bold text-[16px]">
-                            {{ $key }}
-                        </div>
-                        <div class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-8 gap-4">
-                            @foreach ($group as $device)
-                            <a href="{{ route('device.show', ['id' => $device['device_id']]) }}" class="{{ $device['is_line'] == 1 ? 'bg-[#083C76]' : 'bg-gray-500' }} py-2 rounded cursor-pointer hover:scale-105 duration-200 shadow-lg">
-                                <div class="text-gray-300 text-[12px] px-2 truncate">
-                                    {{ $device['point_code'] ?? '-' }}
-                                </div>
-                                <div class="text-gray-300 text-[12px] px-2 truncate">
-                                    {{ $device['address'] ?? '-' }}
-                                </div>
-                                <div class="text-white text-[12px] px-2 truncate mt-2">
-                                    {{ $device['sensor_name'] ?? '-' }}
-                                </div>
-                                <div class="pb-6 text-center text-[24px] text-[#E94F07]">
-                                    {{ $device['value'] ?? '-' }} {{ $device['unit'] ?? '-' }}
-                                </div>
-                            </a>
-                            @endforeach
-                        </div> 
-                    </div>
-                    @endforeach                   
-                </div> --}}
             </div>
         </div>
     </div>
@@ -168,13 +133,13 @@
                             });
 
                             const $deviceGrid = $('<div>', {
-                                class: 'grid grid-cols-2 md:grid-cols-4 lg:grid-cols-8 gap-4'
+                                class: 'grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4'
                             });
 
                             // Iterasi devices
-                            $.each(group, function(index, device) {
+                            $.each(group, function(index, device) {console.log(device.point_code)
                                 const $deviceLink = $('<a>', {
-                                    href: `{{ route('device.show', ['id' => ':id']) }}`.replace(":id", device.device_id),
+                                    href: `{{ route('device.show', ['id' => ':id']) }}`.replace(":id", device.id),
                                     class: (device.is_line === 1 ? 'bg-[#083C76]' : 'bg-gray-500') + ' py-2 rounded cursor-pointer hover:scale-105 duration-200 shadow-lg'
                                 });
 
@@ -188,17 +153,60 @@
                                     text: device.address || '-'
                                 });
 
-                                const $sensorName = $('<div>', {
-                                    class: 'text-white text-[12px] px-2 truncate mt-2',
-                                    text: device.sensor_name || '-'
+                                const $sensorList = $('<div>', {
+                                    class: 'flex items-center justify-center gap-2'
                                 });
 
-                                const $value = $('<div>', {
-                                    class: 'pb-6 text-center text-[24px] text-[#E94F07]',
-                                    text: `${device.value || '-'} ${device.unit || '-'}`
+                                const $divVelocity = $('<div>', {
+                                    class: 'flex flex-col items-center gap-2'
                                 });
 
-                                $deviceLink.append($pointCode, $address, $sensorName, $value);
+                                const $flowVelocityName = $('<div>', {
+                                    class: 'text-white text-[8px] px-2 truncate mt-2',
+                                    text: device.flow_velocity_sensor_name || '-'
+                                });
+
+                                const $flowVelocityValue = $('<div>', {
+                                    class: 'pb-6 text-center text-[10px] text-[#E94F07]',
+                                    text: `${device.flow_velocity_value || '-'} ${device.flow_velocity_unit || '-'}`
+                                });
+
+                                $divVelocity.append($flowVelocityName, $flowVelocityValue);
+
+                                const $divInstantaneous = $('<div>', {
+                                    class: 'flex flex-col items-center gap-2'
+                                });
+
+                                const $instantaneousSensorName = $('<div>', {
+                                    class: 'text-white text-[8px] px-2 truncate mt-2',
+                                    text: device.instantaneous_flow_sensor_name || '-'
+                                });
+
+                                const $instantaneousValue = $('<div>', {
+                                    class: 'pb-6 text-center text-[10px] text-[#E94F07]',
+                                    text: `${device.instantaneous_flow_value || '-'} ${device.instantaneous_flow_unit || '-'}`
+                                });
+
+                                $divInstantaneous.append($instantaneousSensorName, $instantaneousValue);
+
+                                const $divWaterLevel = $('<div>', {
+                                    class: 'flex flex-col items-center gap-2'
+                                });
+
+                                const $waterLevelSensorName = $('<div>', {
+                                    class: 'text-white text-[8px] px-2 truncate mt-2',
+                                    text: device.water_level_sensor_name || '-'
+                                });
+
+                                const $waterLevelValue = $('<div>', {
+                                    class: 'pb-6 text-center text-[10px] text-[#E94F07]',
+                                    text: `${device.water_level_value || '-'} ${device.water_level_unit || '-'}`
+                                });
+
+                                $divWaterLevel.append($waterLevelSensorName, $waterLevelValue);
+
+                                $sensorList.append($divWaterLevel, $divVelocity, $divInstantaneous);
+                                $deviceLink.append($pointCode, $address, $sensorList);
                                 $deviceGrid.append($deviceLink);
                             });
 
